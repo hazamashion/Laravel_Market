28b2cc0af98fb162628779a8c3dbc0fd743efd79
hazamashion <shionn1202@gmail.com>
2023-01-29 18:02:29 +0900

    日本語化エラーメッセージ変更,usersテーブルカラム追加分をUserモデルのに記述

---
+++ database/migrations/2023_01_28_231505_add_columns_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('profile')->default('')->comment('プロフィール');
+            $table->string('image', 100)->default('')->comment('プロフィール画像ファイル名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('profile');
+            $table->dropColumn('image');
+        });
+    }
+}
